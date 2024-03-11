9d2510f0cc6bae0eee88fa13ac87c7883b303533
christopher-duffy-allhuman <102655337+christopher-duffy-allhuman@users.noreply.github.com>
2024-03-11 17:48:34 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,81 @@
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route for GET request
+app.get('/comment', function(req, res) {
+    res.send('Hello, Comment!');
+});
+
+// Start server
+app.listen(port, function() {
+    console.log('Server is running on port 3000');
+});
+
+// Path: index.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route for GET request
+app.get('/', function(req, res) {
+    res.send('Hello, World!');
+});
+
+// Start server
+app.listen(port, function() {
+    console.log('Server is running on port 3000');
+});
+
+// Path: index.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route for GET request
+app.get('/', function(req, res) {
+    res.send('Hello, World!');
+});
+
+// Start server
+app.listen(port, function() {
+    console.log('Server is running on port 3000');
+});
+
+// Path: index.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route for GET request
+app.get('/', function(req, res) {
+    res.send('Hello, World!');
+});
+
+// Start server
+app.listen(port, function() {
+    console.log('Server is running on port 3000');
+});
+
+// Path: index.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route for GET request
+app.get('/', function(req, res) {
+    res.send('Hello, World!');
+});
+
+// Start server
+app.listen(port, function() {
+    console.log('Server is running on port 3000');
+});
+```
+
+###
